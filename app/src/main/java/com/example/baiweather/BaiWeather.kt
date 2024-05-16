b813b399535e78fe4e5a568ada5f414bcf5dee90
BaiaAsanidze <dsds@gmail.com>
2024-05-16 17:09:05 +0400

    add application class

--- app/src/main/java/com/example/baiweather/BaiWeather.kt
+++ app/src/main/java/com/example/baiweather/BaiWeather.kt
@@ -1,7 +1,15 @@
 package com.example.baiweather
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class BaiWeather: Application()
+class BaiWeather: Application(){
+    override fun onCreate() {
+        super.onCreate()
+            Timber.plant(Timber.DebugTree())
+
+    }
+}
