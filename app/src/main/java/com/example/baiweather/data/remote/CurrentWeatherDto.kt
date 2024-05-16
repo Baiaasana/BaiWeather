4945667418e2f3db4cdf88b70e49dabfee31d736
BaiaAsanidze <dsds@gmail.com>
2024-05-16 17:10:30 +0400

    add data classes

--- app/src/main/java/com/example/baiweather/data/remote/CurrentWeatherDto.kt
+++ app/src/main/java/com/example/baiweather/data/remote/CurrentWeatherDto.kt
@@ -3,57 +3,53 @@ package com.example.baiweather.data.remote
 import com.squareup.moshi.Json
 
 data class CurrentWeatherDto(
-    val cod: Int,
-    val message: Int,
-    val name: String,
-    val id: Int,
-    val timezone: Float,
-    val list: List<WeatherInfo>,
+    val cod: Int? = 0,
+    val message: Int? = 0,
+    val name: String? ="",  //city name
+    val id: Int? = 0,
+    val timezone: Float? = 0f,
+    val weather: List<Weather>? = emptyList(),
+    val main: WeatherObject? = WeatherObject(),
+    val wind: Wind? = Wind(),
+    val dt: Long? = 0L,
+    val sys: SysInfo?  = SysInfo(),
     @field:Json(name = "coord")
-    val coordinates: Coordinates
-)
-
-data class WeatherInfo(
-    val main: WeatherObject,
-    val weather: List<Weather>,
-    val pressure: Int,
-    val humidity: Int,
-    val wind: Wind,
-    val visibility: Int,
-    val dt: Long,
-    val sys: SysInfo
+    val coordinates: Coordinates? = Coordinates()
 )
 
 data class WeatherObject(
-    val temp: Float,
+    val temp: Float? = 0f,
     @field:Json(name = "feels_like")
-    val feelsLike: Float,
+    val feelsLike: Float? = 0f,
     @field:Json(name = "temp_min")
-    val tempMin: Float,
+    val tempMin: Float? = 0f,
     @field:Json(name = "temp_max")
-    val tempMax: Float,
+    val tempMax: Float? = 0f,
+    val pressure: Int? = 0,
+    val humidity: Int? = 0
 )
 
 data class Weather(
-    val id: Int,
-    val main: String,
-    val description: String,
-    val icon: String
+    val id: Int?,
+    val main: String?,
+    val description: String?,
+    val icon: String?
 )
 
 data class Wind(
-    val speed: Float,
-    val deg: Int
+    val speed: Float?  =0f,
+    val deg: Int? = 0,
+    val gust: Float? = 0f
 )
 
 data class SysInfo(
-    val country: String,
-    val sunrise: Long,
-    val sunset: Long
+    val country: String? = "",
+    val sunrise: Long? = 0L,
+    val sunset: Long? = 0L
 )
 
 data class Coordinates(
-    val lon: Float,
-    val lat: Float
+    val lon: Float? = 0f,
+    val lat: Float? = 0f
 )
 
