b15a9322e48a90b9b0732b911da2f01a5041e756
BaiaAsanidze <dsds@gmail.com>
2024-05-16 17:08:43 +0400

    add Resource class

--- app/src/main/java/com/example/baiweather/domain/util/Resource.kt
+++ app/src/main/java/com/example/baiweather/domain/util/Resource.kt
@@ -1,7 +1,6 @@
 package com.example.baiweather.domain.util
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    object Loading: Resource<Nothing>()
     class Success<T>(data: T?): Resource<T>(data)
     class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
 }
