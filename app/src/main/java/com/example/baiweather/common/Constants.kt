2086a3eb385ce9d98fe4e123bc8daef0e711472f
BaiaAsanidze <dsds@gmail.com>
2024-05-16 17:09:26 +0400

    add Constants

--- app/src/main/java/com/example/baiweather/common/Constants.kt
+++ app/src/main/java/com/example/baiweather/common/Constants.kt
@@ -3,8 +3,14 @@ package com.example.baiweather.common
 object Constants {
 
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val CURRENT_END_POINT = "data/2.5/weather?appid=385fb0fae782ddd4bfe16a4c8ee131ac"
-    const val HOURLY_END_POINT = "data/2.5/forecast/hourly?appid=385fb0fae782ddd4bfe16a4c8ee131ac"
-    const val DAILY_END_POINT = "data/2.5/forecast/daily?appid=385fb0fae782ddd4bfe16a4c8ee131ac"
+    const val CURRENT_END_POINT = "data/2.5/weather?appid=5a174ab46111459ed0d16f7c54803e6c"
+    const val DAILY_END_POINT = "data/2.5/forecast?appid=5a174ab46111459ed0d16f7c54803e6c"
 
+//    https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5a174ab46111459ed0d16f7c54803e6c
+//    https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&cnt=2&appid=5a174ab46111459ed0d16f7c54803e6c
+
+    const val LAT = "lat"
+    const val LON = "lon"
+    const val CNT = "cnt"
 }
+
